95cd7d324ff3c347c4df08f8178417e235e22737
sefo <fournierse@gmail.com>
2017-10-01 01:33:33 +0200

    initial commit: emergency

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    var bg = document.getElementById('bg');
+    var callme = document.getElementById('callme');
+    var arrow = document.getElementById('arrow');
+    var swipeMe = document.getElementById('swipeMe');
+
+    arrow.addEventListener('click', swipe);
+    callme.addEventListener('click', call);
+    
+    function swipe() {
+        bg.classList.toggle('blurred');
+        swipeMe.classList.toggle('close');
+        arrow.classList.toggle('up');
+    }
+
+    function call() {
+        swipe();
+        console.log("calling...");
+    }
+
+})();
